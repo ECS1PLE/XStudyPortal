5b51f99c1ac753ee324bd11c699cbfd75794942c
ECS1PLE <artyom.vavilov78@gmail.com>
2026-03-21 14:33:00 +0300

    Fix config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,20 +1,9 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import dotenv from "dotenv";
-import { defineConfig } from "prisma/config";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, '.env') });
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-    seed: 'tsx prisma/seed.ts'
-  },
   datasource: {
-    url: process.env.DATABASE_URL ?? '',
-    shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL ?? undefined
-  }
-});
+    url: env('DIRECT_DATABASE_URL'),
+  },
+})
